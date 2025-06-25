4624d74103749432101d81fa00c329cf1409a49c
Raghvendra Mishra <coder.raghvendra@gmail.com>
2025-06-25 18:21:29 +0530

    more changes

--- src/main/java/com/webapp/ms1/tinyurl/controller/TinyUrlController.java
+++ src/main/java/com/webapp/ms1/tinyurl/controller/TinyUrlController.java
@@ -2,7 +2,6 @@ package com.webapp.ms1.tinyurl.controller;
 
 import com.webapp.ms1.tinyurl.service.TinyUrlService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
